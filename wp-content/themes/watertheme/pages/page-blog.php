cfc88058d4f22c705d5721cab211c7996d856dea
gabrielstik <gabriel.stik@gmail.com>
2018-10-29 23:20:23 +0100

    :lipstick: Styled blog header

---
+++ wp-content/themes/watertheme/pages/page-blog.php
@@ -0,0 +1,47 @@
+<?
+/*
+Template Name: Blog
+*/
+?>
+<? get_header() ?>
+<main role="main">
+  <header class="header">
+    <div class="header__top">
+      <div class="header__back">
+        <a href="/quizz" title="Go back to the game">< back to the game
+      </div>
+      <h1 class="header__title">wateryoudoing blog</h1>
+      <div class="header__socials"></div>
+    </div>
+    <nav class="header__nav">
+      <ul class="header__nav__list">
+        <li class="header__nav__list__item current">
+          <a href="#" title="">All</a>
+        </li>
+        <li class="header__nav__list__item">
+          <a href="#" title="">Food</a>
+        </li>
+        <li class="header__nav__list__item">
+          <a href="#" title="">Hygiene</a>
+        </li>
+        <li class="header__nav__list__item">
+          <a href="#" title="">Work</a>
+        </li>
+        <li class="header__nav__list__item">
+          <a href="#" title="">Entertainment</a>
+        </li>
+        <li class="header__nav__list__item">
+          <a href="#" title="">Household</a>
+        </li>
+        <li class="header__nav__list__item">
+          <a href="#" title="">Transport</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="search">
+
+    </div>
+  </header>
+  <div class="blog"></div>
+</main>
+<? get_footer() ?>
