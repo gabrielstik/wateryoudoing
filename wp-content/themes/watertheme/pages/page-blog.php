fa3f29be52156530bc1f070bb9a72a82248d9014
gabrielstik <gabriel.stik@gmail.com>
2018-10-30 12:52:44 +0100

    :art: Styled landing page

--- wp-content/themes/watertheme/pages/page-blog.php
+++ wp-content/themes/watertheme/pages/page-blog.php
@@ -5,43 +5,56 @@ Template Name: Blog
 ?>
 <? get_header() ?>
 <main role="main">
-  <header class="header">
-    <div class="header__top">
-      <div class="header__back">
-        <a href="/quizz" title="Go back to the game">< back to the game
+  <div class="blog">
+    <header class="header">
+      <div class="header__top">
+        <div class="header__back">
+          <a href="/quizz" title="Go back to the game">< back to the game
+        </div>
+        <h1 class="header__title">wateryoudoing blog</h1>
+        <div class="header__socials"></div>
       </div>
-      <h1 class="header__title">wateryoudoing blog</h1>
-      <div class="header__socials"></div>
-    </div>
-    <nav class="header__nav">
-      <ul class="header__nav__list">
-        <li class="header__nav__list__item current">
-          <a href="#" title="">All</a>
-        </li>
-        <li class="header__nav__list__item">
-          <a href="#" title="">Food</a>
-        </li>
-        <li class="header__nav__list__item">
-          <a href="#" title="">Hygiene</a>
-        </li>
-        <li class="header__nav__list__item">
-          <a href="#" title="">Work</a>
-        </li>
-        <li class="header__nav__list__item">
-          <a href="#" title="">Entertainment</a>
-        </li>
-        <li class="header__nav__list__item">
-          <a href="#" title="">Household</a>
-        </li>
-        <li class="header__nav__list__item">
-          <a href="#" title="">Transport</a>
-        </li>
-      </ul>
-    </nav>
-    <div class="search">
-
-    </div>
-  </header>
-  <div class="blog"></div>
+      <nav class="header__nav">
+        <ul class="header__nav__list">
+          <li class="header__nav__list__item current">
+            <a href="#" title="">All</a>
+          </li>
+          <li class="header__nav__list__item">
+            <a href="#" title="">Food</a>
+          </li>
+          <li class="header__nav__list__item">
+            <a href="#" title="">Hygiene</a>
+          </li>
+          <li class="header__nav__list__item">
+            <a href="#" title="">Work</a>
+          </li>
+          <li class="header__nav__list__item">
+            <a href="#" title="">Entertainment</a>
+          </li>
+          <li class="header__nav__list__item">
+            <a href="#" title="">Household</a>
+          </li>
+          <li class="header__nav__list__item">
+            <a href="#" title="">Transport</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="header__search">
+        <input type="text" name="blog-search" id="blog-search" class="header__search__input" placeholder="Rechercher">
+      </div>
+    </header>
+    <section class="blog__blog">
+      <article class="blog__article--first">
+        <div class="blog__article--first__content">
+          <h2 class="blog__article--first__title">About porks</h2>
+          <p class="blog__article--first__author">By Julia Gioviana</p>
+          <p class="blog__article--first__excerpt">So strongly and metaphysically did I conceive of my situation then, that while earnestly watching his motions, I seemed distinctly to perceive that my own individuality was now merged in a joint stock company of two.</p>
+        </div>
+        <div class="blog__article--first__image">
+          <img src="<?= get_template_directory_uri() ?>/images/article-1.jpg" alt="">
+        </div>
+      </article>
+    </section>
+  </div>
 </main>
 <? get_footer() ?>
---
+++ wp-content/themes/watertheme/pages/page-landing.php
@@ -0,0 +1,16 @@
+<?
+/*
+Template Name: Landing
+*/
+?>
+<? get_header() ?>
+<main role="main">
+  <div class="landing">
+    <div class="landing__content">
+      <h1 class="landing__title">Wateryoudoing?</h1>
+      <p class="landing__text">So strongly and metaphysically did I conceive of my situation then, that while earnestly.</p>
+      <a href="/quizz" title="Go to quizz" class="landing__cta">Let's try</a>
+    </div>
+  </div>
+</main>
+<? get_footer() ?>
