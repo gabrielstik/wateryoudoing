077aefe07b84f1b26404778e7fbd407464833360
maudrochel <rochel.maud@gmail.com>
2018-11-21 16:38:13 +0100

    homepage

--- wp-content/themes/watertheme/pages/page-landing.php
+++ wp-content/themes/watertheme/pages/page-landing.php
@@ -6,10 +6,22 @@ Template Name: Landing
 <? get_header() ?>
 <main role="main">
   <div class="landing">
-    <div class="landing__content">
-      <h1 class="landing__title">Wateryoudoing?</h1>
-      <p class="landing__text">So strongly and metaphysically did I conceive of my situation then, that while earnestly.</p>
-      <a href="/quizz" title="Go to quizz" class="landing__cta">Let's try</a>
+    <div class="landing__left">
+      <h1 class="landing__title_left">
+        Water<br>you<br>doing
+      </h1>
+    </div>
+    <div class="landing__right">
+      <img src="<?= get_template_directory_uri() ?>/images/wateryoudoing_logo.svg" alt="wateryoudoing" class="landing__logo">
+      <p class="landing__text">
+      An adult consumes an average of 160 liters of water a day.
+      </p>
+      <p class="landing__text2">
+      Beyond its own consumption, water is used to produce a lot of things... Wateryoudoing wants to confront you to your own reality.
+      </p>
+      <div class="landing__button">
+        <a href="/quizz/" class="landing__button_inside">C'est parti !</a>
+      </div>
     </div>
   </div>
 </main>
