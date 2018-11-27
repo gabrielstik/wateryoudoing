4f8e5d3803b729ab3625e687b3f05312e98ae117
maudrochel <rochel.maud@gmail.com>
2018-11-27 16:01:20 +0100

    fun fact page

---
+++ wp-content/themes/watertheme/pages/page-funfact.php
@@ -0,0 +1,16 @@
+<?
+/*
+Template Name: Fun Fact
+*/
+?>
+<? get_header() ?>
+<main role="main">
+    <div class="funfact">
+        <div class="funfact__content">
+            <img class="funfact__content__img" src="<?= get_template_directory_uri() ?>/images/dream.svg" alt="dream">
+            <h1 class="funfact__content__title">Let’s have a break !</h1>
+            <p class="funfact__content__text">You worked a lot ! Have a little nap now.</p>
+            <div class="funfact__content__button"><a href=# class="funfact__content__link">Ok...</a></div>
+        </div>
+    </div>
+</main>
