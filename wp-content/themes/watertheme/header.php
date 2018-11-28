bba3b46f5f57433de11d7ebedfb5868cbeaba9db
gabrielstik <gabriel.stik@gmail.com>
2018-11-28 15:09:21 +0100

    fixed bug

--- wp-content/themes/watertheme/header.php
+++ wp-content/themes/watertheme/header.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html <? language_attributes() ?>>
 <head>
-  <title>wateryoudoing</title>
+  <title><? the_title() ?></title>
   <meta charset="<? bloginfo('charset') ?>" />
   <link rel="stylesheet" href="<? bloginfo('stylesheet_url') ?>">
   <link rel="icon" type="image/png" href="<? THEME_ROOT.'/images/favicon.png' ?>" />
